c9c91cda80514e8ed42fb9c15297d4020faae5cf
Yann Esposito <yann.esposito@gmail.com>
2012-06-15 11:56:48 +0200

    cleaned some warning by adding type signatures

--- 06_Mandelbulb/Mandel.hs
+++ 06_Mandelbulb/Mandel.hs
@@ -3,7 +3,7 @@ module Mandel (mandel) where
 
 import ExtComplex
 
-mandel :: GLfloat -> GLfloat -> GLfloat -> Int -> Int
+mandel :: Float -> Float -> Float -> Int -> Int
 mandel r i s nbIterations = 
     f (extcomplex r i s) 0 nbIterations
     where
